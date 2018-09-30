2af83df0ca5f083df881f3022f5170fc7d8fb825
Alexey Tomilov <tomilovlehach@mail.ru>
2018-10-01 00:00:52 +0600

    Added train and test images to future system

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$index = 0;
+$path_to_images = "C:/Users/Apofeo/Downloads/CropNumbers/NumBase";
+$dir_items = array_diff( scandir( $path_to_images ), array('..', '.') );
+foreach ($dir_items as $dir_item) :
+  $index++;
+  $dir_item = explode(".", $dir_item);
+  $new_dir_items[] = $dir_item[0];
+  $size = getimagesize($path_to_images . "/" . $dir_item[0] . "." . $dir_item[1]);
+  $tags = ($index > 4056) ? "test" : "train";
+  $data = array(
+    'tags' => [$tags,$tags],
+    'description' => '',
+    'objects' => [],
+    'size' => array(
+      'height' => $size[1],
+      'width' => $size[0],
+    ),
+  );
+  $formattedData = json_encode($data);
+  $filename = $dir_item[0] . ".json";
+  $handle = fopen($tags . "/" . $filename,'w+');
+  fwrite($handle,$formattedData);
+  fclose($handle);
+endforeach;
+// echo count($dir_items);
+// sort($dir_items);
+// var_dump($new_dir_items);
+// var_dump($sizes);
+?>
---
+++ test.php
@@ -0,0 +1,19 @@
+{
+  "tags": ["test", "test"],
+  "description": "A007HA50",
+  "objects": [],
+  "size": {
+    "height": 34,
+    "width": 152
+  }
+}
+
+{
+  "tags": ["train", "train"],
+  "description": "",
+  "objects": [],
+  "size": {
+    "height": "",
+    "width": ""
+  }
+}
