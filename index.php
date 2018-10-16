c07af4353ed4885d126ee0b81f77e080631aeed6
Alexey Tomilov <tomilovlehach@mail.ru>
2018-10-17 00:19:18 +0600

    Added images and labels for train and test base

--- index.php
+++ index.php
@@ -4,24 +4,25 @@ $path_to_images = "C:/Users/Apofeo/Downloads/CropNumbers/NumBase";
 $dir_items = array_diff( scandir( $path_to_images ), array('..', '.') );
 foreach ($dir_items as $dir_item) :
   $index++;
-  $dir_item = explode(".", $dir_item);
-  $new_dir_items[] = $dir_item[0];
-  $size = getimagesize($path_to_images . "/" . $dir_item[0] . "." . $dir_item[1]);
+  // $dir_item = explode(".", $dir_item);
+  // $new_dir_items[] = $dir_item[0];
+  // $size = getimagesize($path_to_images . "/" . $dir_item[0] . "." . $dir_item[1]);
   $tags = ($index > 4056) ? "test" : "train";
-  $data = array(
-    'tags' => [$tags,$tags],
-    'description' => '',
-    'objects' => [],
-    'size' => array(
-      'height' => $size[1],
-      'width' => $size[0],
-    ),
-  );
+  // $data = array(
+  //   'tags' => [$tags,$tags],
+  //   'description' => '',
+  //   'objects' => [],
+  //   'size' => array(
+  //     'height' => $size[1],
+  //     'width' => $size[0],
+  //   ),
+  // );
   // $formattedData = json_encode($data, JSON_UNESCAPED_UNICODE);
   // $filename = $dir_item[0] . ".json";
   // $handle = fopen($tags . "/" . $filename,'w+');
   // fwrite($handle,$formattedData);
   // fclose($handle);
+  // rename($path_to_images . "/" . $dir_item, $path_to_images . "/" . $tags . "/" . $dir_item);
 endforeach;
 // echo count($dir_items);
 // sort($dir_items);
